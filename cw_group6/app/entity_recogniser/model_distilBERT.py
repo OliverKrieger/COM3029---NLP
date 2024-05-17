d9a4d9db485343527523a89a3420aea30054563e
Oliver Krieger <oliver.krieger76@gmail.com>
2024-05-18 00:47:14 +0100

    add distilBERT as entity recogniser

--- cw_group6/app/entity_recogniser/model_distilBERT.py
+++ cw_group6/app/entity_recogniser/model_distilBERT.py
@@ -1,2 +1,21 @@
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+
+
+from utils.paths import distilBERT_model
+
+tag_list = ["B-O", "B-AC", "B-LF", "I-LF"]
+id2label = [{idx:tag} for idx, tag in enumerate(tag_list)]
+label2id = [{tag:idx} for idx, tag in enumerate(tag_list)]
+
+tokenizer = AutoTokenizer.from_pretrained(distilBERT_model)
+model = AutoModelForTokenClassification.from_pretrained(distilBERT_model)
+
+from transformers import pipeline
+
+classifier = pipeline("ner", model=distilBERT_model)
+
+
 def distilBERT_recognise_entities(input:str):
-    return ""
+    print("OUTPUT:", classifier(input))
+
+    return [(ent["word"], ent["entity"]) for ent in classifier(input)]
