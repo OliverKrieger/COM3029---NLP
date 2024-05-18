63c7dc38ee527a4af8dcb820a568f3ca5985b77b
Oliver Krieger <oliver.krieger76@gmail.com>
2024-05-18 15:36:37 +0100

    styling on textbox, write function names for unit tests - WIP

---
+++ cw_group6/app/unit_tests/test_runner.py
@@ -0,0 +1,22 @@
+def test_input():
+    print()
+
+def test_long_input():
+    print()
+
+def test_no_input():
+    print()
+
+def test_array_input():
+    print()
+
+def test_JSON_input():
+    print()
+
+def test_invalid_array_input():
+    print()
+
+def test_invalid_JSON_input():
+    # has to have tokens fields
+    # ner tags fields
+    print()
