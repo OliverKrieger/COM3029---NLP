645ae2ce2b7bc26af32e315904ed59259d97382e
Oliver Krieger <oliver.krieger76@gmail.com>
2024-05-18 02:29:57 +0100

    remove unused code

--- cw_group6/app/entity_recogniser/model_distilBERT.py
+++ cw_group6/app/entity_recogniser/model_distilBERT.py
@@ -1,11 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
-
-
 from utils.paths import distilBERT_model
-
-tag_list = ["B-O", "B-AC", "B-LF", "I-LF"]
-id2label = [{idx:tag} for idx, tag in enumerate(tag_list)]
-label2id = [{tag:idx} for idx, tag in enumerate(tag_list)]
 
 tokenizer = AutoTokenizer.from_pretrained(distilBERT_model)
 model = AutoModelForTokenClassification.from_pretrained(distilBERT_model)
