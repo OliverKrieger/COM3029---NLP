4aad92bbf66a0fbf3fc9577a95c2bea6f98dff17
Oliver Krieger <oliver.krieger76@gmail.com>
2024-05-18 19:09:51 +0100

    assertation errors

--- cw_group6/app/unit_tests/test_runner.py
+++ cw_group6/app/unit_tests/test_runner.py
@@ -7,18 +7,22 @@ def test_input():
         input_str = "OBS is an abbreviation"
         response = run_model("spaCy", input_str)
         assert response == [('OBS', 'AC'), ('is', 'O'), ('an', 'O'), ('abbreviation', 'O')]
+    except AssertionError:
+        raise Exception("ERROR (test_input) - assertation failed!")
     except Exception:
-        print("ERROR (test_input) - assertation failed!")
         print(traceback.format_exc())
+        raise Exception("ERROR (test_input) - test failed for non assertation reasons!")
 
 def test_long_input():
     try:
         input_str = "So far, reviews investigating insulin pump therapy showed mixed results regarding the health - related quality of life (HRQOL) in paediatric diabetes patients.[13, 16, 17] According to recent publications,[15, 18–20] there is still a lack of adequately powered studies to underpin the advantages of CSII regarding QoL improvement for children diagnosed with DM and to potentially balance the higher treatment cost attached to it.[8] Thus, insulin pumps are – forty years after they were first introduced to the market – still not part of first - line recommendations in most countries around the world."
         response = run_model("spaCy", input_str)
         assert response == [('So', 'O'), ('far', 'O'), (',', 'O'), ('reviews', 'O'), ('investigating', 'O'), ('insulin', 'O'), ('pump', 'O'), ('therapy', 'O'), ('showed', 'O'), ('mixed', 'O'), ('results', 'O'), ('regarding', 'O'), ('the', 'O'), ('health - related quality of life', 'LF'), ('(', 'O'), ('HRQOL', 'AC'), (')', 'AC'), ('in', 'O'), ('paediatric', 'O'), ('diabetes', 'O'), ('patients.[13', 'O'), (',', 'O'), ('16', 'O'), (',', 'O'), ('17', 'O'), (']', 'O'), ('According', 'O'), ('to', 'O'), ('recent', 'O'), ('publications,[15', 'O'), (',', 'O'), ('18–20', 'O'), (']', 'O'), ('there', 'O'), ('is', 'O'), ('still', 'O'), ('a', 'O'), ('lack', 'O'), ('of', 'O'), ('adequately', 'O'), ('powered', 'O'), ('studies', 'O'), ('to', 'O'), ('underpin', 'O'), ('the', 'O'), ('advantages', 'O'), ('of', 'O'), ('CSII', 'AC'), ('regarding', 'O'), ('QoL', 'AC'), ('improvement', 'O'), ('for', 'O'), ('children', 'O'), ('diagnosed', 'O'), ('with', 'O'), ('DM', 'AC'), ('and', 'O'), ('to', 'O'), ('potentially', 'O'), ('balance', 'O'), ('the', 'O'), ('higher', 'O'), ('treatment', 'O'), ('cost', 'O'), ('attached', 'O'), ('to', 'O'), ('it.[8', 'O'), (']', 'O'), ('Thus', 'O'), (',', 'O'), ('insulin', 'O'), ('pumps', 'O'), ('are', 'O'), ('–', 'O'), ('forty', 'O'), ('years', 'O'), ('after', 'O'), ('they', 'O'), ('were', 'O'), ('first', 'O'), ('introduced', 'O'), ('to', 'O'), ('the', 'O'), ('market', 'O'), ('–', 'O'), ('still', 'O'), ('not', 'O'), ('part', 'O'), ('of', 'O'), ('first', 'O'), ('-', 'O'), ('line', 'O'), ('recommendations', 'O'), ('in', 'O'), ('most', 'O'), ('countries', 'O'), ('around', 'O'), ('the', 'O'), ('world', 'O'), ('.', 'O')]
+    except AssertionError:
+        raise Exception("ERROR (test_long_input) - assertation failed!")
     except Exception:
-        print("ERROR (test_long_input) - assertation failed!")
         print(traceback.format_exc())
+        raise Exception("ERROR (test_long_input) - test failed for non assertation reasons!")
 
 def test_no_input():
     try:
@@ -26,9 +30,11 @@ def test_no_input():
         assert response == []
         response = run_model("DistilBERT", "")
         assert response == []
+    except AssertionError:
+        raise Exception("ERROR (test_no_input) - assertation failed!")
     except Exception:
-        print("ERROR (test_no_input) - assertation failed!")
         print(traceback.format_exc())
+        raise Exception("ERROR (test_no_input) - test failed for non assertation reasons!")
 
 def test_array_input():
     print()
